d2bfc1ae89ad51ec782633c6b0a6fe9e788f9c25
Aleksander <kewgine@gmail.com>
2022-08-17 21:42:20 +0300

    приве в норму репозиторий и поработал в папке example002

---
+++ Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string username = Console.ReadLine();
+            Console.Write("Привет, ");
+            Console.Write(username);
+        }
+    }
+}
