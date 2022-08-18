058ace4fb9db2641d068ce53fbab48ac2099f001
Aleksander <kewgine@gmail.com>
2022-08-18 18:27:35 +0300

    add conditionelse

---
+++ Example005/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string username = Console.ReadLine();
+            if (username.ToLower() == "маша")
+            { 
+                Console.WriteLine("Ура, наконец-то пришла Маша!");
+            }
+            else
+            {
+                Console.WriteLine("Привет, " + username);
+            }
+        }
+    }
+}
