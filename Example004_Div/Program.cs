a5af9bf44715345f0725147eac1ca7dfdd585e0c
Aleksander <kewgine@gmail.com>
2022-08-18 17:32:51 +0300

    add random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -6,9 +6,12 @@ namespace Example004__Div
     {
         static void Main(string[] args)
         {
-            double numberA = 12;
-            double numberB = 5;
-            Console.WriteLine(numberA/numberB);
+            int numberA = new Random().Next(1,10);
+            Console.WriteLine(numberA); 
+            int numberB = new Random().Next(1,10);
+            Console.WriteLine(numberB);
+            int result = numberA + numberB;
+            Console.WriteLine(result);
 
         }
     }
