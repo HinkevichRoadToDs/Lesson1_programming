e2e56622e38ba397908650608d98b63bfa0a3e6e
Aleksander <kewgine@gmail.com>
2022-08-18 18:57:19 +0300

    add example006

---
+++ Example006/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 13;
+            int b = 3;
+            int c = 7;
+            int d = 3;
+            int e = 12;
+
+            int max_number = a;
+
+            if (b > max_number) max_number = b;
+            if (c > max_number) max_number = c;
+            if (d > max_number) max_number = d;
+            if (e > max_number) max_number = e;
+            
+            Console.WriteLine(max_number);
+        }
+    }
+}
